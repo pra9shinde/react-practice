1166f577bca53bd4d8a71add243fdb242496287d
Pranav Shinde <pranavshnd006@gmail.com>
2021-07-27 19:35:13 +0530

    Review Slider

---
+++ Review-Slider/src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'susan smith',
+    job: 'web developer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    text:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi quis quaerat earum similique, saepe vitae voluptates non voluptatibus, repellendus ut est possimus molestiae iste dolorem mollitia deleniti consectetur recusandae laborum.",
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    job: 'web designer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+    text:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi quis quaerat earum similique, saepe vitae voluptates non voluptatibus, repellendus ut est possimus molestiae iste dolorem mollitia deleniti consectetur recusandae laborum.',
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    job: 'intern',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    text:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi quis quaerat earum similique, saepe vitae voluptates non voluptatibus, repellendus ut est possimus molestiae iste dolorem mollitia deleniti consectetur recusandae laborum.',
+  },
+  {
+    id: 4,
+    name: 'bill anderson',
+    job: 'the boss',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    text:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi quis quaerat earum similique, saepe vitae voluptates non voluptatibus, repellendus ut est possimus molestiae iste dolorem mollitia deleniti consectetur recusandae laborum. ',
+  },
+];
+
+export default reviews;
